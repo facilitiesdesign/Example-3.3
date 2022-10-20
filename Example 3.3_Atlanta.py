8e26fe8c8e930ff20937c3acae235c78f81693b9
Sunderesh S. Heragu <102760654+facilitiesdesign@users.noreply.github.com>
2022-10-20 13:14:42 -0500

    Add files via upload

---
+++ Example 3.3_Atlanta.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 17 10:22:03 2022
+
+@author: grace_elizabeth
+"""
+
+from gurobipy import *
+
+try:
+    
+    #Create lists
+    plant = ['Albany', 'Little Rock', 'Atlanta']
+    warehouse = ['Boston', 'Philadephia', 'Galveston', 'Raleigh']
+    supply = [250, 300, 880]
+    demand = [200, 100, 300, 280]
+    cost = [ 
+            [10, 15, 22, 20], #Albany plant
+            [19, 15, 10, 9], #Little Rock plant
+            [21, 11, 13, 6], #Atlanta plant
+            ]
+    #indices
+    P = len(plant)
+    W = len(warehouse)
+    
+    #Make dictionaries
+    supply_dict = {plant[i]: supply[i] for i in range(P)}
+    demand_dict = {warehouse[j] : demand[j] for j in range(W)}
+    cost_dict = {(plant[i], warehouse[j]) : cost[i][j] for i in range(P) for j in range(W)}
+    
+    #Create model
+    m = Model("Example 3.3")
+    
+    x = m.addVars(range(P), range(W), lb = 0, vtype = GRB.CONTINUOUS, name = "Volume")
+    
+    #Set objective fuction
+    m.setObjective(quicksum(cost[i][j] * x[i,j] for i in range(P) for j in range(W)), GRB.MINIMIZE)
+    
+      #Write constraints
+    for i in range(P):
+        m.addConstr(quicksum(x[i,j] for j in range(W)) <= supply[i], name = "Supply_Constraint")
+                    
+    for j in range(W):
+        m.addConstr(quicksum(x[i,j] for i in range(P)) >= demand[j], name = "Demand_Constraint")
+        
+    #Call Gurobi Optimizer
+    m.optimize()
+    if m.status == GRB.OPTIMAL:
+       for v in m.getVars():
+           if v.x > 0:
+               print('%s = %g' % (v.varName, v.x)) 
+       print('Obj = %f' % m.objVal)
+    elif m.status == GRB.INFEASIBLE:
+       print('LP is infeasible.')
+    elif m.status == GRB.UNBOUNDED:
+       print('LP is unbounded.')
+except GurobiError:
+    print('Error reported')
